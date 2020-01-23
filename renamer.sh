763ded44ce0493f4e481ef42a088338898e162cd
hongsea <hongsea119@gmail.com>
2020-01-23 11:17:12 +0700

    update

--- renamer.sh
+++ renamer.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Disliked name"
-VAR1=yourdomain
+read VAR1
 echo "New Name"
 read VAR2
 grep -rli $VAR1 ./test/* | xargs -i@ sed -i s/$VAR1/$VAR2/g @
-# rename s/$VAR1/$VAR2/ **
+rename s/$VAR1/$VAR2/ **
--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-# read VAR1 VAR2
-# echo $VAR1 $VAR2
-# grep -rli yourdomain ./test/* | xargs -i@ sed -i s/yourdomain/$VAR2/g @
+read VAR1 VAR2
+echo $VAR1 $VAR2
+grep -rli $VAR1 ./test/* | xargs -i@ sed -i s/$VAR1/$VAR2/g @
