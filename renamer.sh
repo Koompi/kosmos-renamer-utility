213dba0fd2d456ce4b61138d266c5a5f6698e149
hongsea <hongsea119@gmail.com>
2020-01-23 11:12:35 +0700

    udpate

--- renamer.sh
+++ renamer.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Disliked name"
-read VAR1
+VAR1=yourdomain
 echo "New Name"
 read VAR2
 grep -rli $VAR1 ./test/* | xargs -i@ sed -i s/$VAR1/$VAR2/g @
-rename s/$VAR1/$VAR2/ **
+# rename s/$VAR1/$VAR2/ **
